f186fc68ad60b820f5ff5aa64943684dce9f639f
hilmy194 <m.hilmy.mahardika@gmail.com>
2025-05-04 23:48:09 +0700

    done

--- app.js
+++ app.js
@@ -11,22 +11,19 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  
+  origin: [
+    "https://localhost:5173", 
+    "https://tutam9-frontend-hilmy.vercel.app" 
+  ],
+  credentials: true,
 }));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/transactions', authenticate, transactionRoutes);
-app.get("/", (req, res) => {
-  res.send("Server is running");
-});
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
-    const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+    app.listen(process.env.PORT, () => console.log('Server running on port', process.env.PORT));
   })
   .catch(err => console.error('MongoDB connection error:', err));

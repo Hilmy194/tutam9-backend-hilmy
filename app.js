4e181ad6f813294f09a4cdbbbe71fe467c981d13
hilmy194 <m.hilmy.mahardika@gmail.com>
2025-05-04 23:03:23 +0700

    tutam9

--- app.js
+++ app.js
@@ -20,6 +20,10 @@ app.use('/api/transactions', authenticate, transactionRoutes);
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => console.log('Server running on port', process.env.PORT));
+    const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
   })
   .catch(err => console.error('MongoDB connection error:', err));
